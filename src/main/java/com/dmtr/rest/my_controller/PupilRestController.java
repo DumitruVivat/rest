269227ce9eec1945c1e6505b26ca703e2e84fcc6
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-12 14:26:48 +0300

    add pupil rest handler

--- src/main/java/com/dmtr/rest/my_controller/PupilRestController.java
+++ src/main/java/com/dmtr/rest/my_controller/PupilRestController.java
@@ -2,9 +2,10 @@ package com.dmtr.rest.my_controller;
 
 import com.dmtr.rest.entity.Pupil;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,20 +37,10 @@ public class PupilRestController {
     public Pupil getPupilByIndexOfArrayList(@PathVariable int pupilIndex) {
 
         if (pupilIndex < 0 || pupilIndex >= pupils.size()) {
-            throw new PupilNotFoundException("Pupils id " + pupilIndex + " not found.");
+            throw new PupilNotFoundException("Pupil id " + pupilIndex + " not found.");
         }
 
         return pupils.get(pupilIndex);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<PupilErrorResponse>  handlerException(PupilNotFoundException exception){
-        PupilErrorResponse error = new PupilErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exception.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 }
